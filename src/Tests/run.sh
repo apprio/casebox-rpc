b74472b772945b7d5db68b9ebffd6169ad0a9bbe
Vladimir Melnic <win32hack@gmail.com>
2016-05-13 12:49:01 +0300

    Tests refactoring.

--- src/Tests/run.sh
+++ src/Tests/run.sh
@@ -2,7 +2,23 @@
 
 set -e
 
+function rmcache () {
+    if [ -d $CACHE ]; then
+        sudo rm -r $CACHE
+    fi
+}
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PHP=$( which php )
 
-$PHP $DIR/../../../../bin/phpunit --colors --verbose --debug --configuration $DIR/phpunit.xml
+CACHE=$DIR/../../../../../var/cache/test
+
+rmcache "$CACHE"
+
+$PHP $DIR/../../../../bin/phpunit --colors --verbose --debug --configuration $DIR/phpunit-app.xml
+
+rmcache "$CACHE"
+
+$PHP $DIR/../../../../bin/phpunit --colors --verbose --debug --configuration $DIR/phpunit-api.xml
+
+rmcache "$CACHE"
